0da71f627a861de357515e24e609675839c14821
Zac022 <i.rudasingwa@alustudent.com>
2023-01-21 13:36:49 -0800

    init

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,23 @@
 #!/usr/bin/python3
-"""
-Module: base.py
-"""
-import models
 import uuid
-from datetime import datetime
+import datetime
 
-
-class BaseModel():
-    """
-    Base class which defines all common
-    attributes/methods for other classes
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        instatiates an object with it's
-        attributes
-        """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
+class BaseModel:
+    def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        models.storage.new(self)
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """
-        Returns the string representation
-        of the instance
-        """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
-        self.updated_at = datetime.now()
-        models.storage.save()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
-        dict = {**self.__dict__}
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
+        return_dict = self.__dict__.copy()
+        return_dict["__class__"] = self.__class__.__name__
+        return_dict["created_at"] = self.created_at.isoformat()
+        return_dict["updated_at"] = self.updated_at.isoformat()
+        return return_dict
 
-        return dict
