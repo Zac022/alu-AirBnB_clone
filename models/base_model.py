324fec978ce92618a53949378958775cb46f1f99
Zac022 <i.rudasingwa@alustudent.com>
2023-01-21 13:31:53 -0800

    changes made

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        return_dict = self.__dict__.copy()
-        return_dict["__class__"] = self.__class__.__name__
-        return_dict["created_at"] = self.created_at.isoformat()
-        return_dict["updated_at"] = self.updated_at.isoformat()
-        return return_dict
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
 
