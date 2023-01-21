94d8881c6e5413b7433df5a0b408344abacebbd1
Zac022 <i.rudasingwa@alustudent.com>
2023-01-21 12:46:37 -0800

    tests

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        return_dict = self.__dict__.copy()
+        return_dict["__class__"] = self.__class__.__name__
+        return_dict["created_at"] = self.created_at.isoformat()
+        return_dict["updated_at"] = self.updated_at.isoformat()
+        return return_dict
+
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.bm = BaseModel()
+        self.bm.name = "My First Model"
+        self.bm.my_number = 89
+
+    def test_save(self):
+        before_save = self.bm.updated_at
+        self.bm.save()
+        after_save = self.bm.updated_at
+        self.assertNotEqual(before_save, after_save)
+
+    def test_to_dict(self):
+        bm_dict = self.bm.to_dict()
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
+        self.assertEqual(bm_dict["created_at"], self.bm.created_at.isoformat())
+        self.assertEqual(bm_dict["updated_at"], self.bm.updated_at.isoformat())
+        self.assertEqual(bm_dict["id"], self.bm.id)
+        self.assertEqual(bm_dict["name"], self.bm.name)
+        self.assertEqual(bm_dict["my_number"], self.bm.my_number)
+
+if __name__ == "__main__":
+    unittest.main()
+
