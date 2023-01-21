cf8ef122a3a8e45f931b2ed73226ad634bb0b35b
Zac022 <i.rudasingwa@alustudent.com>
2023-01-21 12:59:30 -0800

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel:
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+
     def save(self):
         self.updated_at = datetime.datetime.now()
 
