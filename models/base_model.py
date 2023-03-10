ad41bc89f4664bb0c39df44b6b829689145f2c2a
Gatwaza <gatwazarobert177@gmail.com>
2023-03-10 01:54:09 -0800

    ugh

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,66 @@
 #!/usr/bin/python3
+""" this is the base class for all models """
 import uuid
-import datetime
-from base_model import BaseModel
+from datetime import date, datetime, time
+from models import storage
+
+
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    """class defining commom attributes
+    for other classes"""
+
+    def __init__(self, *argv, **kwargs):
+        """ instance attributes
+        id : string
+        created_at: datetime
+        updated_at: datetime
+        getting arguments to recreate
+        BaseModel
+        creates new instance Model
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = \
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = value
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ default string output of class name \
+                ,id and dictionary files """
+        return '[{}] ({}) {}'\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        """
+        update of attribute updated_at
+        with latest time
+        updated_at: datetime
+        """
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        return_dict = self.__dict__.copy()
-        return_dict["__class__"] = self.__class__.__name__
-        return_dict["created_at"] = self.created_at.isoformat()
-        return_dict["updated_at"] = self.updated_at.isoformat()
-        return return_dict
-
+        """
+        get the objects of the instance
+        returns: dictionary
+        """
+        temp_dict = self.__dict__.copy()
+        temp_dict['__class__'] = self.__class__.__name__
+        temp_dict['created_at'] = temp_dict['created_at'].isoformat()
+        temp_dict['updated_at'] = temp_dict['updated_at'].isoformat()
+        return temp_dict
